e685c72db019f2ed6152f6db81f8a58ab3059dc8
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2026-01-01 02:00:38 +0100

    chore: core functionality for users

---
+++ apps/users/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    '''Serializer for user registration with password validation.'''
+
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name', 'password', 'password2', 'role')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    '''Serializer for user details.'''
+
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'full_name', 'role', 'created_at')
+        read_only_fields = ('id', 'created_at')
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .serializers import UserRegistrationSerializer, UserSerializer
+
+User = get_user_model()
+
+class UserRegistrationView(generics.CreateAPIView):
+    '''API endpoint for user registration.'''
+
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            'success': True,
+            'message': 'User registered successfully',
+            'data': UserSerializer(user).data
+        }, status=status.HTTP_201_CREATED)
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    '''API endpoint for retrieving and updating user profile.'''
+
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
